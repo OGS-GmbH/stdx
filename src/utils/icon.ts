94abcdf15bcfd837fbc3337bc2f69dce13c6bd7e
Elie Soueid <eliegebransoued@gmail.com>
2025-10-22 08:01:47 +0200

    chore: remove files that are unrelated to a standard lib

--- src/utils/icon.ts
+++
@@ -1,9 +0,0 @@
-import { IconName } from "@fortawesome/pro-light-svg-icons";
-
-function mapPrefixNameToIconName (iconName: string): IconName {
-  return iconName.split("fa-").pop() as IconName;
-}
-
-export {
-  mapPrefixNameToIconName
-};
